a975ca9b82d81d5ade21220885d39d3ec449e66a
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-04 10:36:17 +1300

    Create 00_tkinter_template.py

---
+++ 00_tkinter_template.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random
+
+
+class Converter:
+    def __init__(self, parent):
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Converter(root)
+    root.mainloop()
