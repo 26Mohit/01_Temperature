eb805c8c90198c77821159d187724ee46345a589
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-10 12:16:28 +1300

    Working Help

--- 01_Help_GUI_v4.py
+++ 01_Help_GUI_v4.py
@@ -4,18 +4,35 @@ from functools import partial   # To prevent unwanted windows
 import random
 
 
-class Converter
+class Converter:
     def __init__(self, parent):
+
        # Formatting variables...
-       background _color= "light blue"
+       background_color= "light blue"
 
        # Converter Main screen GUI...
        self.coverter_frame =Frame(width=600, height=600, bg=background_color)
        self.coverter_frame.grid()
 
+       # Temperature Conversion Heading (row 0)
+       self.temp_coverter_label + Label(self.coverter_frame, text="temperature Converter",
+                                        font="Arial 16 bold",
+                                            bg=background_color,
+                                        padx=10, pady=10)
+       self.temp_coverter_label.grid(row=0)
+
+        # Help Button (row 1)
+        self.help_button = Button(self.converter_frame, text="help",
+                                  padx=10, pady=10)
+        self.help_button.grid(row=1)
 
 
-# main routine
+ def help(self):
+     print("You asked for help)")
+     get_help = Help(self)
+     get_help.help_text.configure(text="help text goes here")
+
+# main routine:
 if __name__ == "__main__":
     root = Tk()
     root.title("Temperature Converter")
