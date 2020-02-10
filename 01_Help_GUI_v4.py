0b77a575acd9952eee42cc102f30f80baf868bd6
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-11 10:38:53 +1300

    Update 01_Help_GUI_v4.py

--- 01_Help_GUI_v4.py
+++ 01_Help_GUI_v4.py
@@ -7,30 +7,41 @@ import random
 class Converter:
     def __init__(self, parent):
 
-       # Formatting variables...
-       background_color= "light blue"
+        # Formatting variables...
+        background_color= "light blue"
 
-       # Converter Main screen GUI...
-       self.coverter_frame =Frame(width=600, height=600, bg=background_color)
-       self.coverter_frame.grid()
+        # Converter Main screen GUI...
+        self.converter_frame =Frame(width=600, height=600, bg=background_color)
+        self.converter_frame.grid()
 
-       # Temperature Conversion Heading (row 0)
-       self.temp_coverter_label + Label(self.coverter_frame, text="temperature Converter",
-                                        font="Arial 16 bold",
-                                            bg=background_color,
-                                        padx=10, pady=10)
-       self.temp_coverter_label.grid(row=0)
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(self.converter_frame, text="temperature Converter",
+                                font="Arial 16 bold",
+                                    bg=background_color,
+                                padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
 
         # Help Button (row 1)
-        self.help_button = Button(self.converter_frame, text="help",
-                                  padx=10, pady=10)
+        self.help_button = Button(self.converter_frame, text="help", padx=10, pady=10)
         self.help_button.grid(row=1)
 
 
- def help(self):
-     print("You asked for help)")
-     get_help = Help(self)
-     get_help.help_text.configure(text="help text goes here")
+    def Help(self):
+         print("You asked for help)")
+         get_help = Help(self)
+         get_help.help_text.configure(text="help text goes here")
+
+class Help:
+    def __init__(self,partner):
+
+        background = "orange"
+
+        # disable help button
+        partner.help_button.config(state=DISABLED)
+
+
+
+
 
 # main routine:
 if __name__ == "__main__":
