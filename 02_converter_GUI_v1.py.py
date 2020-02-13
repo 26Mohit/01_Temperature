3c08f24750ef8485a662d9cdea03764a453040ef
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-13 14:46:50 +1300

    Create 02_converter_GUI_v1.py.py

---
+++ 02_converter_GUI_v1.py.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables
+        background_color = "light blue"
+
+        # Converter Frame
+
+        # Temperature Converter Heading (row 0)
+
+        # User instruction (row 1)
+
+        # Temperature entry box (row 2)
+
+        # Conversion buttons frame (row 3)
+
+        # Answer label (row 4)
+        print("hello world")
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("title goes here")
+    something = Foo(root)
+    root.mainloop()
