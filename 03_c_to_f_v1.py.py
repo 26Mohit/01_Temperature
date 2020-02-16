fd05f7dd28b563d52de0cd51dbb228f3f24cde04
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-17 12:16:16 +1300

    Create 03_c_to_f_v1.py.py

---
+++ 03_c_to_f_v1.py.py
@@ -0,0 +1,19 @@
+# quick component to convert degrees C To F.
+# Function takes in value, does conversion and puts answer into a list
+
+
+def to_f(from_c):
+    fahrenheit = (from_c * 9/5) + 32
+    return fahrenheit
+
+
+# Main routine
+temperatures = [0, 40, 100]
+converter = []
+
+for item in temperatures:
+    answer = to_f(item)
+    ans_statement = "{} degrees C is {} degrees F". format(item, answer)
+    converter.append(ans_statement)
+
+print converter
