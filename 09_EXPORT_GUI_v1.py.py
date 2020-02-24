416508d0a74de92a9956e54fa46690174f25defe
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-25 10:25:30 +1300

    Update 09_EXPORT_GUI_v1.py.py

--- 09_EXPORT_GUI_v1.py.py
+++ 09_EXPORT_GUI_v1.py.py
@@ -1,15 +1,32 @@
 from tkinter import *
+from functools import partial   # To prevent unwanted windows
+
 import random
 
+if __name__ == '__main__':
+    class Converter:
+        if __name__ == '__main__':
+            def __init__(self):
 
-class Converter
-    def __init__(self, parent):
-        print("hello world")
+                # Formatting variables...
+                background_color = "light blue"
+
+                # In actual program this is blank and is populated witrh user calculations
+                self.all_clac_list = ['0 degrees C is -17.8 degrees F',
+                                      '0 degrees C is 32 degrees F',
+                                      '40 degrees C is 104 degrees F',
+                                      '40 degrees C is 4.4 degrees F',
+                                      '12 degrees C is 53.6 degrees F',
+                                      '24 degrees C is 75.2 degrees F',
+                                      '100 degrees C is 37.8 degrees F']
+
+                # Converter Main Screen GUI..
+                self.converter_frame = Frame(width=300, height=300, bg=background_color,
+                                             pady=10)
+                self.converter_frame.grid()
+
+                # Temperature Conversion Heading (row=0)
 
 
-# main routine
-if __name__ == "__main__":
-    root = Tk()
-    root.title("Temperature Converter")
-    something = Converter(root)
-    root.mainloop()
+
+
