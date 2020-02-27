781300d294e1f7a7e6a3f8a17d02ec3d85aabab6
26Mohit <60586130+26Mohit@users.noreply.github.com>
2020-02-28 12:41:44 +1300

    assembled converter

--- 02_converter_GUI_v3.py.py
+++ 02_converter_GUI_v3.py.py
@@ -70,6 +70,9 @@ class Converter:
                                      command=lambda: self.get_history(self.all_calc_list))
         self.history_button.grid(row=0, column=0)
 
+        if len(self.all_calc_list) ==0:
+            self.history_button.config(state=DISABLED)
+
         self.help_button = Button(self.hist_help_frame, font="Arial 12 bold",
                                   text="Help", width=5)
         self.help_button.grid(row=0, column=1)
